c80c235c212fc081b076f3751591eab97519209c
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-07-22 09:47:17 +0100

    deleted random folder

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar() {
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Link to="/">BornAtMidknight</Link>
+          <Link to="/">Born At MidKnight</Link>
 
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
